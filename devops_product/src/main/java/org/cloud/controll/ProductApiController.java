ce52599e0fca32d782d30d8d4e6802607dc1b93e
yeeonk1 <jyk5152532@gmail.com>
2026-04-09 11:47:34 +0900

    devops1 v1.1

--- devops_product/src/main/java/org/cloud/controll/ProductApiController.java
+++ devops_product/src/main/java/org/cloud/controll/ProductApiController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// github test pull
 @RestController
 @RequestMapping("/api/product")
 @CrossOrigin(origins = "http://localhost:5173")
